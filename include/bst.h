924d5a93ea40c1bd8e4b8c06cca740f4949b6dc1
lisyonkovaa <114025777+lisyonkovaa@users.noreply.github.com>
2023-06-01 08:52:51 +0000

    upd 1

--- include/bst.h
+++ include/bst.h
@@ -1,5 +1,79 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_BST_H_
 #define INCLUDE_BST_H_
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+
+template <typename T>
+class BST {
+ private:
+    struct Node {
+        T value;
+        int count;
+        Node* left, * right;
+    };
+    int s;
+    Node* root;
+    Node* addNode(Node* root, const T& value) {
+        if (!root) {
+            root = new Node;
+            root->value = value;
+            root->count = 1;
+            root->left = root->right = nullptr;
+        } else if (root->value > value) {
+            root->left = addNode(root->left, value);
+        } else if (root->value < value) {
+            root->right = addNode(root->right, value);
+        } else {
+            root->count++;
+        }
+        return root;
+    }
+
+    int depthTree(Node* root) {
+        if (!root)
+            return 0;
+        return mymax(depthTree(root->left), depthTree(root->right)) + 1;
+    }
+
+    int mymax(int a, int b) {
+        if (a > b)
+            return a;
+        else
+            return b;
+    }
+
+ public:
+    BST() :root(nullptr), s(0) {}
+    void add(const T& value) {
+        root = addNode(root, value);
+    }
+
+        int search(const T& value) {
+        searchTree(root, value);
+        return s;
+    }
+
+    void searchTree(Node* root, const T& value) {
+        if (!root)
+            s = 0;
+        else if (root->value > value)
+            searchTree(root->left, value);
+        else if (root->value < value)
+            searchTree(root->right, value);
+        else
+            s = root->count;
+    }
+
+    int depth() {
+        int d = depthTree(root);
+        return d - 1;
+    }
+};
+
+BST<std::string> makeTree(const char* filename);
+std::string get_corr_word(std::string str);
 
 #endif  // INCLUDE_BST_H_
--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,68 @@
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+    BST<std::string> Mytree;
+    std::ifstream file(filename);
+    std::string word;
+    std::string word1 = "";
+    std::string word2 = "";
+    while (!file.eof()) {
+        file >> word;
+        word1 = word2 = "";
+        if (w.find(",") != -1) {
+                int length = word.length();
+                int find = word.find(",");
+                if ((find != length - 1) && ((word[find + 1] >= 'A' && word[find + 1] <= 'Z') ||
+                    (word[find + 1] >= 'A' && word[find + 1] <= 'z'))) {
+                for (int item = 0; item < find; item++) {
+                    word1 += word[item];
+                }
+                for (int item = find+1; item < length; item++) {
+                    word2 += word[item];
+                }
+            }
+        }
+        if (word1 != "" && word2 != "") {
+            word1 = get_corr_word(word1);
+            word2 = get_corr_word(word2);
+            Mytree.add(word1);
+            Mytree.add(word2);
+        } else {
+            w = get_corr_word(word);
+            Mytree.add(word);
+        }
+    }
+    return Mytree;
+}
+
+std::string get_corr_word(std::string s) {
+    if (s[0] < '0' || s[0] > '9') {
+        while ((s[0] >= 32 && s[0] <= 64) || (s[0] >= 91 && s[0] <= 96))
+        s.erase(0, 1);
+        if (s != "") {
+            char c = s[s.length() - 1];
+            while ((c >= 32 && c <= 64) || (c >= 91 && c <= 96)) {
+                s.pop_back();
+                c = s[s.length() - 1];
+            }
+        }
+        int it = 0;
+        if (s != "") {
+            for (int i = 0; i < s.length(); i++) {
+                if (s[i] == '\'') {
+                    it = i;
+                }
+            }
+            if (it != 0)
+                s.erase(it);
+        }
+        if (s != "") {
+            for (int i = 0; i < s.length(); i++) {
+                s[i] = tolower(s[i]);
+            }
+        }
+    } else {
+        s.clear();
+    }
+    return s;
 }
